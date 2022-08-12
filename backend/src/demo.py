5eef387b50198040fb43bf867955d3c7018a0903
Luca Vivona <lucavivona01@gmail.com>
2022-08-12 13:27:59 -0400

    refactor 💻

--- backend/src/demo.py
+++ backend/src/demo.py
@@ -10,4 +10,4 @@ def add(x, y):
     return x + y
 
 if __name__ == "__main__":
-    tabularGradio([Hello_World(), add()], ["Hello World",])
+    tabularGradio([Hello_World(), add()], ["Hello World", "Add"])
